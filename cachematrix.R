d294b7380d9e0e7c849db171b82fc3a668e8d7ae
Fabiano Silva <fabiano.silva@gmail.com>
2014-06-21 17:03:33 -0300

    adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions bellow are designed to calculate the inverse of a given matrix
+## In case the matrix has already been calculated a cached value will be sent.
 
-## Write a short comment describing this function
+## This funcion creates the "bnase" matrix for which a inverse can be 
+## calculated
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
+  m <- NULL                                   #Setting the initial value
+  set <- function(y) {                        # In case a new value is 
+    x <<- y                                   # passed, cache is cleared
     m <<- NULL
   }
   get <- function() x
@@ -18,16 +19,17 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This funcion calculates the inverse matrix for the object created
+## by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    return(m)
+  m <- x$getinverse()                         # try to retrive cache
+  if(!is.null(m)) {                           #In case cache is valid
+    return(m)                                 #return value retrieved
   }
-  data <- x$get()
-  m <- solve(data)
-  x$setinverse(m)
-  m
+  data <- x$get()                             #In case cache is null, get data
+  m <- solve(data)                            #calculates the inverse
+  x$setinverse(m)                             #updates object/cache
+  m                                           #returns inverse
 }
